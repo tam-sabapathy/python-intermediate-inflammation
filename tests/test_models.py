0330393c6d8f2969a1294029220779423c1e4c3c
Tam Sabapathy <tamilarasan.sabapathy@stfc.ac.uk>
2022-11-28 16:49:27 +0000

    added more example test_models

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,7 +8,8 @@ import pytest, pytest_cov
     "test, expected",
     [
         ([[0, 0],[0, 0],[0, 0]], [0,0]),
-        ([[1, 2],[3, 4],[5, 6]], [3,4])
+        ([[1, 2],[3, 4],[5, 6]], [3,4]),
+        ([[0, 0],[0, 0],[0, 0]], [0,0])
     ]
 )
 def test_daily_mean_integers(test, expected):
