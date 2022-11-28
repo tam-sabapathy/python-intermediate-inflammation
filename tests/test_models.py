0511968fe9a59b9ecae780e0ecdb3833481d03ab
Tam Sabapathy <tamilarasan.sabapathy@stfc.ac.uk>
2022-11-28 15:36:00 +0000

    changed yml, model, testmodel.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -51,3 +51,14 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min(['abd','ads'], ['asd', 'suhs'])
+
+# @pytest.mark.parametrize(
+#     'test, expected'
+#     [
+#         ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+#     ]
+# )
+# def test_patient_normalise(test, expected):
+
+#     from inflammation.models import patient_normalise
+#     npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
